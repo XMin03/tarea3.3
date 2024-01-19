92b15011d47a4275f53e84d6161f9739a0642537
Mattia Lu <mlu481@g.educaand.es>
2024-01-19 10:56:28 +0100

    3.5hecho bindingResult fallo

--- src/main/java/org/iesvdm/tarea3_3/model/Pedido.java
+++ src/main/java/org/iesvdm/tarea3_3/model/Pedido.java
@@ -2,14 +2,11 @@ package org.iesvdm.tarea3_3.model;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.iesvdm.tarea3_3.dao.PedidoDAO;
-import org.iesvdm.tarea3_3.dao.PedidoDAOImpl;
-import org.springframework.jdbc.core.JdbcTemplate;
-
 import java.sql.Date;
 @Data
 @AllArgsConstructor
@@ -17,13 +14,14 @@ import java.sql.Date;
 public class Pedido {
     /*parametros de la base de datos*/
     int id;
-
+    @NotNull
     @NotNull(message = "Total no puede ser nulo.")
     @Min(value = 0,message = "Minimo {value} ")
     double total;
 
     @NotNull(message = "Fecha no puede ser nulo.")
     @NotBlank(message = "Fecha no puede ser vacío.")
+    @NotEmpty(message = "Fecha no puede ser vacío.")
     Date fecha;
 
     @NotNull(message = "Id_cliente no puede ser nulo.")
